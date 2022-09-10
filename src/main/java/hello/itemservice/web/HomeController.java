89fc5cff13839a05401fe12baa0065f5ea447801
kaaang <kaaang@kakao.com>
2022-09-11 04:49:06 +0900

    test commit

--- src/main/java/hello/itemservice/web/HomeController.java
+++ src/main/java/hello/itemservice/web/HomeController.java
@@ -1,7 +1,9 @@
 package hello.itemservice.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +13,10 @@ public class HomeController {
     @RequestMapping("/")
     public String home() {
         return "redirect:/items";
+    }
+
+    @RequestMapping("/test")
+    public ResponseEntity test(){
+        return ResponseEntity.ok("test");
     }
 }
